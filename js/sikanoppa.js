e9a8cfcf57c4ad4d255e5ee8524e5f886306e6c5
ioyli <90900819+ioyli@users.noreply.github.com>
2022-02-07 16:33:19 +0200

    Add files via upload

--- js/sikanoppa.js
+++ js/sikanoppa.js
@@ -42,6 +42,22 @@ function Pelaaja(nimi, pisteet, vuoro) {
     this.vuoro = vuoro;
 }
 
+// määritä pistetilanne
+function pistetilanne() {
+    let text = "";
+
+    for (let i = 0; i < pelaajaMaara; i++) {
+        text = text + `${pelaajaLista[i].nimi}: <span id="${pelaajaLista[i].nimi}Pisteet">${pelaajaLista[i].pisteet}</span><br>`;
+    }
+
+    document.getElementById("pistetilanne").innerHTML = `${text}`;
+}
+
+// päivitä pistetilanne
+function pistetilanneUpdate(pelaaja) {
+    document.getElementById(`${pelaajaLista[pelaaja].nimi}Pisteet`).innerHTML = `${pelaajaLista[pelaaja].pisteet}`;
+}
+
 function finalize() {
     for (let i = 0; i < pelaajaMaara; i++) {
         let pelaaja = document.getElementById(`pelaajaNimi${i + 1}`).value;
@@ -51,7 +67,19 @@ function finalize() {
     document.getElementById("setup").style.display = "none";
     document.getElementById("game").style.display = "block";
 
+    if (nopat == 1) {
+        document.getElementById("noppaB").style.display = "none";
+    }
+
+    pistetilanne();
     refreshPlayer(0);
+    
+}
+
+function refreshPlayer(vuoro) {
+    document.getElementById("vuoro").innerHTML = `${pelaajaLista[vuoro].nimi}`;
+    document.getElementById("vuoroPisteet").innerHTML = `${pisteet}`;
+    document.getElementById("pelaajanPisteet").innerHTML = `${pelaajaLista[vuoro].pisteet}`;
 }
 
 // SIKANOPPA
@@ -60,6 +88,7 @@ let pisteet = 0;
 let tuplatPerakkain = 0;
 
 function heita() {
+
     if (vuoro == pelaajaMaara) {
         vuoro = 0;
     }
@@ -76,10 +105,12 @@ function heita() {
     
         refreshPlayer(vuoro);
         document.getElementById("huom").innerHTML = `Heitit ${noppa}!`;
+        document.getElementById("noppaA").innerHTML = `<img src="img/noppa${noppa}.png">`;
     
         // vuoro loppuu ja pisteet menetetään jos heitto on 1
         if (noppa == 1) {
             document.getElementById("huom").innerHTML = `Heitit ${noppa}! Menetit heittämäsi pisteet ja vuoro vaihtuu.`;
+            document.getElementById("noppaA").innerHTML = `<img src="img/noppa${noppa}.png">`;
             pisteet = 0;
             vuoro++;
         }
@@ -98,42 +129,51 @@ function heita() {
 
             if (tuplatPerakkain == 3) {
                 document.getElementById("huom").innerHTML = `Heitit tuplat peräkkäin kolmesti! Menetit heittämäsi pisteet ja vuoro vaihtuu.`;
+                document.getElementById("noppaA").innerHTML = `<img src="img/noppa${noppa}.png">`;
+                document.getElementById("noppaB").innerHTML = `<img src="img/noppa${noppaB}.png">`;
                 refreshPlayer(vuoro);
                 pisteet = 0;
                 vuoro++;
+                document.getElementById("noppaA").innerHTML = "";
+                document.getElementById("noppaB").innerHTML = "";
             } else {
                 document.getElementById("huom").innerHTML = `Heitit ${noppa} ja ${noppaB}! Saat tuplapisteet.`;
+                document.getElementById("noppaA").innerHTML = `<img src="img/noppa${noppa}.png">`;
+                document.getElementById("noppaB").innerHTML = `<img src="img/noppa${noppaB}.png">`;
                 refreshPlayer(vuoro);
             }
         } else if (noppa == 1 || noppaB == 1) {
             document.getElementById("huom").innerHTML = `Heitit ${noppa} ja ${noppaB}! Vain toinen on 1, joten menetit heittämäsi pisteet ja vuoro vaihtuu.`;
+            document.getElementById("noppaA").innerHTML = `<img src="img/noppa${noppa}.png">`;
+            document.getElementById("noppaB").innerHTML = `<img src="img/noppa${noppaB}.png">`;
             refreshPlayer(vuoro);
             pisteet = 0;
             vuoro++;
         } else {
             document.getElementById("huom").innerHTML = `Heitit ${noppa} ja ${noppaB}!`;
+            document.getElementById("noppaA").innerHTML = `<img src="img/noppa${noppa}.png">`;
+            document.getElementById("noppaB").innerHTML = `<img src="img/noppa${noppaB}.png">`;
             pisteet += noppa + noppaB;
             refreshPlayer(vuoro);
         }
     }
 }
 
-function refreshPlayer(vuoro) {
-    document.getElementById("vuoro").innerHTML = `${pelaajaLista[vuoro].nimi}`;
-    document.getElementById("vuoroPisteet").innerHTML = `${pisteet}`;
-    document.getElementById("pelaajanPisteet").innerHTML = `${pelaajaLista[vuoro].pisteet}`;
-}
-
 // vuoro loppuu ja pisteet lisätään kun itse niin päättää
 function lopetaVuoro() {
     pelaajaLista[vuoro].pisteet += pisteet;
+    pistetilanneUpdate(vuoro);
 
     if (pelaajaLista[vuoro].pisteet >= maxPisteet) {
         let winner = `Onneksi olkoon, ${pelaajaLista[vuoro].nimi}! Keräsit eniten pisteitä, ja voitit pelin!`
-        document.getElementById("game").innerHTML = winner + `<br> <button onclick="window.location.reload(true)">Uusi peli</button>`;
+        document.getElementById("gameInner").innerHTML = winner + `<br> <button onclick="window.location.reload(true)">Uusi peli</button>`;
     } else {
         pisteet = 0;
         vuoro++;
+        document.getElementById("noppaA").innerHTML = "";
+        if (nopat == 2) {
+            document.getElementById("noppaB").innerHTML = "";
+        }
 
         if (vuoro == pelaajaMaara) {
             vuoro = 0;
